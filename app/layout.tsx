013d2c718737d82558fae0fd53beee43f94d549b
RiverNguyen <ngiang1309@gmail.com>
2024-07-26 22:57:50 +0700

    update user active

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,30 +3,32 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ToasterContext from "./context/ToasterContext";
 import AuthContext from "./context/AuthContext";
+import ActiveStatus from "./components/ActiveStatus";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Messenger",
-    description: "Messenger",
+  title: "Messenger",
+  description: "Messenger",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="icon" type="image/png" href="/images/logo.png" />
-            </head>
-            <body className={inter.className}>
-                <AuthContext>
-                    <ToasterContext />
-                    {children}
-                </AuthContext>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/images/logo.png" />
+      </head>
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  );
 }
---
+++ pages/api/pusher/auth.ts
@@ -0,0 +1,24 @@
+import authOptions from "@/app/libs/authOptions";
+import { pusherServer } from "@/app/libs/pusher";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session?.user?.email) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  const socketId = req.body.socket_id;
+  const channel = req.body.channel_name;
+  const data = {
+    user_id: session.user.email,
+  };
+
+  const authRes = pusherServer.authorizeChannel(socketId, channel, data);
+
+  return res.send(authRes);
+};
+
+export default handler;
